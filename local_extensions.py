3f5707a8711ffe9ec99acb8c6b65fd9d4a91e28a
Matheus Girardi <matheus.girardi@brf.com>
2025-04-17 14:18:44 +0000

    Added a few QoL changes

--- local_extensions.py
+++ local_extensions.py
@@ -8,6 +8,9 @@ ADDITIONAL_PROMPTS = {
     "full_name":  "  [1/3] Your name",
     "email": "  [2/3] Your email",
     "use_git": "  [3/3] Would you like to initialize a git repository?",
+
+    "save_mail":"[Extra] No email or a different one was found in git config, do you want to save this email to it?",
+    "save_name":"[Extra] No name or a different one was found in git config, do you want to save this name to it?"
 }
 
 
@@ -66,6 +69,29 @@ class AdditionalPrompts(Extension):
         def prompt_user_yes_no(var_name, default=True):
             return read_user_yes_no(var_name, default, prompts=ADDITIONAL_PROMPTS)
 
+        def ask_to_save(info, type):
+            if type == "email":
+                to_save = prompt_user_yes_no("save_mail")
+                if to_save:
+                    try:
+                        subprocess.check_call(['git', 'config', '--global',  '--add', 'user.email', info], 
+                                              stdout=subprocess.DEVNULL,
+                                              stderr=subprocess.DEVNULL)
+                    except subprocess.CalledProcessError as e:
+                        print(f"Error saving name: {e}")
+                    
+            elif type == "name":
+                to_save = prompt_user_yes_no("save_name")
+                try:
+                    subprocess.check_call(['git', 'config', '--global',  '--add', 'user.name', info],
+                                           stdout=subprocess.DEVNULL,
+                                           stderr=subprocess.DEVNULL)
+                except subprocess.CalledProcessError as e:
+                    print(f"Error saving name: {e}")
+            else:
+                print(f"Not a valid type {type}.")
+        
+
         # Make these functions available to the Jinja2 context
         environment.globals.update(
             prompt_user=prompt_user,
@@ -74,4 +100,5 @@ class AdditionalPrompts(Extension):
             branch_name=branch_name,
             get_user_name=get_user_name,
             get_user_email=get_user_email,
+            ask_to_save=ask_to_save,
         )
